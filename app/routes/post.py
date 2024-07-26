0557e075dd205652d181f0cf1a60e03a49e14bed
Pranav Aher <doingdifferent29@gmail.com>
2024-07-26 13:09:47 +0530

    Modify get_current_user action to find user from database based on id in token
    Rename user_id to current_user in post routes

--- app/routes/post.py
+++ app/routes/post.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 @router.get("/", response_model = List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" SELECT * FROM posts """)
   # posts = cursor.fetchall()
 
@@ -19,7 +19,7 @@ def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_c
   return posts
 
 @router.post("/", status_code = status.HTTP_201_CREATED, response_model = schemas.PostResponse)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
   # new_post = cursor.fetchone()
   # connection.commit()
@@ -32,7 +32,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id
   return new_post
 
 @router.get("/{id}", response_model = schemas.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
   # post = cursor.fetchone()
 
@@ -44,7 +44,7 @@ def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oaut
   return post 
     
 @router.put("/{id}", status_code = status.HTTP_200_OK, response_model = schemas.PostResponse)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
   # updated_post = cursor.fetchone()
   # connection.commit()
@@ -60,7 +60,7 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
   return post.first()
 
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id)))
   # deleted_post = cursor.fetchone()
   # connection.commit()
