f8a68cb5e5813493adc6657c4ed3a38481872500
Pranav Aher <doingdifferent29@gmail.com>
2024-07-26 15:33:29 +0530

    Set logged in user on post while creating post

--- app/routes/post.py
+++ app/routes/post.py
@@ -24,7 +24,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
   # new_post = cursor.fetchone()
   # connection.commit()
 
-  new_post = models.Post(**post.model_dump())
+  new_post = models.Post(owner_id = current_user.id, **post.model_dump())
   db.add(new_post)
   db.commit()
   db.refresh(new_post) # Get newly created post in new_post
