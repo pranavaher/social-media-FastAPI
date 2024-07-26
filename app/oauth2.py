7fa31de53b780f29064c426bed4fb76d67715934
Pranav Aher <doingdifferent29@gmail.com>
2024-07-26 10:06:02 +0530

    Fix bug

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,7 +13,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
   to_encode = data.copy()
 
-  expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+  expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
   to_encode.update({ "exp": expire })
 
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -22,7 +22,7 @@ def create_access_token(data: dict):
 
 def verify_access_token(token: str, credentials_exception):
   try:
-    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
     id: str = payload.get("user_id")
 
