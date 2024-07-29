d21dd663abc4c395d66b8cd1857bd72af70ca942
Pranav Aher <doingdifferent29@gmail.com>
2024-07-29 15:37:58 +0530

    Fetch count of likes along with single post

--- app/routes/post.py
+++ app/routes/post.py
@@ -21,8 +21,6 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 
   posts = db.query(models.Post, func.count(models.Vote.post_id).label("likes")).join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-  print(db.query(models.Post, func.count(models.Vote.post_id).label("likes")).join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip))
-
   results = []
   for post, likes in posts:
     post_dict = {
@@ -56,12 +54,14 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
 
   return new_post
 
-@router.get("/{id}", response_model = schemas.PostResponse)
+@router.get("/{id}", response_model = schemas.PostLikeResponse)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
   # post = cursor.fetchone()
 
-  post = db.query(models.Post).filter(models.Post.id == id).first()
+  # post = db.query(models.Post).filter(models.Post.id == id).first()
+
+  post, likes = db.query(models.Post, func.count(models.Vote.post_id).label("likes")).join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first() 
 
   if not post:
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Post with id {id} not found") 
@@ -69,6 +69,23 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
   if post.owner_id != current_user.id:
     raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = f"You are not authorized to perform this action")
   
+  post_dict = {
+      'id': post.id,
+      'title': post.title,
+      'content': post.content,
+      'published': post.published,
+      'created_at': post.created_at,
+      'owner_id': post.owner_id,
+      'owner': {
+        'id': post.owner.id,
+        'email': post.owner.email,
+        'created_at': post.owner.created_at
+      },
+      'likes': likes
+    }
+  
+  post = post_dict
+    
   return post 
     
 @router.put("/{id}", status_code = status.HTTP_200_OK, response_model = schemas.PostResponse)
