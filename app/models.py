2d01a71f3978598e9eaec221ee45216973944a6e
Pranav Aher <doingdifferent29@gmail.com>
2024-07-30 17:36:04 +0530

    Auto add phone number and updated_at column to users and posts table using alembic

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Post(Base):
   content = Column(String, nullable=False)
   published = Column(Boolean, server_default="True", nullable=False)
   created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+  updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
   owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
   owner = relationship("User")
@@ -21,6 +22,7 @@ class User(Base):
   email = Column(String, nullable=False, unique=True)
   password = Column(String, nullable=False)
   created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+  phone_number = Column(String)
 
 class Vote(Base):
   __tablename__ = "votes"
