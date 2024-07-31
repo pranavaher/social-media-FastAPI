3f34bc1da19a37fb69a20330ff1ff8965f5d653a
Pranav Aher <doingdifferent29@gmail.com>
2024-07-31 16:05:01 +0530

    Setup CORS policy for API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routes import post, user, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine) # Can be commentted because we are using alembic
 
 app = FastAPI()
+
+origins = [ "*" ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def hello():
+  return {"message": "cdcd" }
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
